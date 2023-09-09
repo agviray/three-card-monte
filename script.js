7602a4aeaed6bc3e58f63e7903c97ce520798ffc
Allan Viray <allang.v.dev@gmail.com>
2023-09-08 19:49:03 -0500

    Add initial click event listeners to title-screen buttons

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hello three card monte!');
+const howToPlayButton = document.querySelector(
+  '.title-screen__button-options__instructions'
+);
+const playGameButton = document.querySelector(
+  '.title-screen__button-options__play'
+);
+
+// - Shows instructions to user.
+const showInstructions = () => {
+  console.log('Instructions are shown!');
+};
+
+// - Progresses user to main game.
+// - Exits title-screen and tilts environment forward to display the top
+//   of the box (where the cards are laying).
+const playGame = () => {
+  console.log('Envionment tilts forward, and main game scene is shown!');
+};
+
+const loadEventListeners = () => {
+  console.log('loadEventListeners ran');
+  howToPlayButton.addEventListener('click', showInstructions);
+  playGameButton.addEventListener('click', playGame);
+};
+
+loadEventListeners();
