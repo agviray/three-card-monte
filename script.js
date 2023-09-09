ee829a9cbde5768364c96c8751ffdcbd002bf33c
Allan Viray <allang.v.dev@gmail.com>
2023-09-08 20:40:25 -0500

    Add exit-instructions button to instructions

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const howToPlayButton = document.querySelector(
-  '.title-screen__button-options__instructions'
+const showInstructionsButton = document.querySelector(
+  '.button--show-instructions'
 );
-const playGameButton = document.querySelector(
-  '.title-screen__button-options__play'
+const playGameButton = document.querySelector('.button--play-game');
+const exitInstructionsButton = document.querySelector(
+  '.button--exit-instructions'
 );
 
 // - Shows instructions to user.
 const showInstructions = () => {
   console.log('Instructions are shown!');
+};
+
+// - Exits out of instructions.
+const exitInstructions = () => {
+  console.log('Instructions are exited!');
 };
 
 // - Progresses user to main game.
@@ -19,7 +25,8 @@ const playGame = () => {
 
 const loadEventListeners = () => {
   console.log('loadEventListeners ran');
-  howToPlayButton.addEventListener('click', showInstructions);
+  showInstructionsButton.addEventListener('click', showInstructions);
+  exitInstructionsButton.addEventListener('click', exitInstructions);
   playGameButton.addEventListener('click', playGame);
 };
 
