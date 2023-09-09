8fbaead73646f740b29ce6e952bffe52ad4b7efe
Allan Viray <allang.v.dev@gmail.com>
2023-09-08 21:49:39 -0500

    Update dialogue content and styles, and add initial click listeners to buttons

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const showInstructionsButton = document.querySelector(
-  '.button--show-instructions'
+const showInstructionsButtons = Array.from(
+  document.querySelectorAll('.button--show-instructions')
 );
 const playGameButton = document.querySelector('.button--play-game');
 const exitInstructionsButton = document.querySelector(
   '.button--exit-instructions'
 );
+const rearrangeCardsButton = document.querySelector('.button--rearrange-cards');
 
 // - Shows instructions to user.
 const showInstructions = () => {
@@ -23,11 +24,19 @@ const playGame = () => {
   console.log('Envionment tilts forward, and main game scene is shown!');
 };
 
+// - Rearrange/mix the three cards
+const rearrangeCards = () => {
+  console.log('Cards are rearranged!');
+};
+
 const loadEventListeners = () => {
   console.log('loadEventListeners ran');
-  showInstructionsButton.addEventListener('click', showInstructions);
+  showInstructionsButtons.forEach((button) =>
+    button.addEventListener('click', showInstructions)
+  );
   exitInstructionsButton.addEventListener('click', exitInstructions);
   playGameButton.addEventListener('click', playGame);
+  rearrangeCardsButton.addEventListener('click', rearrangeCards);
 };
 
 loadEventListeners();
